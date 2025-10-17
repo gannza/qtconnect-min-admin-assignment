47aff32a32422f0057356353273efb9afb1c71fa
Ganza <respinho2014@gmail.com>
2025-10-17 21:40:32 +0200

    adding typo check

---
+++ node_modules/QTConnect-admin-backend/eslint.config.js
@@ -0,0 +1,106 @@
+const jsdoc = require('eslint-plugin-jsdoc');
+
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        console: 'readonly',
+        Buffer: 'readonly',
+        global: 'readonly',
+        setTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearTimeout: 'readonly',
+        clearInterval: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly'
+      }
+    },
+    plugins: {
+      jsdoc
+    },
+    settings: {
+      jsdoc: {
+        mode: 'typescript',
+        tagNamePreference: {
+          param: 'param',
+          returns: 'returns',
+          augments: 'extends'
+        }
+      }
+    },
+    rules: {
+      // Code style rules
+      'no-console': 'on',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'class-methods-use-this': 'off',
+      'max-len': ['error', { code: 120, ignoreComments: true }],
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'space-before-function-paren': ['error', 'never'],
+      'no-underscore-dangle': 'off',
+      'consistent-return': 'off',
+      'no-param-reassign': 'off',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'error',
+      'arrow-spacing': 'error',
+      'no-duplicate-imports': 'error',
+      'no-useless-constructor': 'error',
+      'no-useless-return': 'error',
+      'prefer-template': 'error',
+      'template-curly-spacing': 'error',
+      'object-shorthand': 'error',
+      'prefer-destructuring': ['error', {
+        array: true,
+        object: true
+      }, {
+        enforceForRenamedProperties: false
+      }],
+      // JSDoc rules
+      'jsdoc/require-jsdoc': ['error', {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true
+        }
+      }],
+      'jsdoc/require-param': 'error',
+      'jsdoc/require-param-description': 'error',
+      'jsdoc/require-param-type': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-returns-description': 'error',
+      'jsdoc/require-returns-type': 'error',
+      'jsdoc/check-param-names': 'error',
+      'jsdoc/check-tag-names': 'error',
+      'jsdoc/check-types': 'error',
+      'jsdoc/valid-types': 'error'
+    }
+  },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    rules: {
+      'no-unused-expressions': 'off',
+      'jsdoc/require-jsdoc': 'off'
+    }
+  }
+];
